34f02ab091fc7dc7dc9375feeff8d07e4208405f
yibu.wang <yibu.wang@gooddoctor.com.sg>
2021-02-24 17:15:42 +0800

    update code

--- config_tina/home.ts
+++ config_tina/home.ts
@@ -90,49 +90,97 @@ export const formOptions: any = {
       uploadDir,
       previewSrc,
     },
-    // {
-    //   name: "animation_title_3",
-    //   component: "text",
-    // },
-    // {
-    //   name: "animation_description_3",
-    //   component: "text",
-    // },
-    // {
-    //   name: "animation_image_3",
-    //   component: "image",
-    //   uploadDir,
-    //   previewSrc,
-    // },
-    // {
-    //   name: "our_products",
-    //   component: "text",
-    // },
-    // {
-    //   name: "our_product_title_1",
-    //   component: "text",
-    // },
-    // {
-    //   name: "our_product_description_1",
-    //   component: "text",
-    // },
-    // {
-    //   name: "our_product_image_1",
-    //   uploadDir,
-    //   previewSrc,
-    // },
-    // {
-    //   name: "our_product_title_2",
-    //   component: "text",
-    // },
-    // {
-    //   name: "our_product_description_2",
-    //   component: "text",
-    // },
-    // {
-    //   name: "our_product_image_2",
-    //   uploadDir,
-    //   previewSrc,
-    // },
+    {
+      name: "animation_title_3",
+      component: "text",
+    },
+    {
+      name: "animation_description_3",
+      component: "text",
+    },
+    {
+      name: "animation_image_3",
+      component: "image",
+      uploadDir,
+      previewSrc,
+    },
+    {
+      name: "our_products",
+      component: "text",
+    },
+    {
+      name: "our_product_title_1",
+      component: "text",
+    },
+    {
+      name: "our_product_description_1",
+      component: "text",
+    },
+    {
+      name: "our_product_image_1",
+      component: "image",
+      uploadDir,
+      previewSrc,
+    },
+    {
+      name: "our_product_title_2",
+      component: "text",
+    },
+    {
+      name: "our_product_description_2",
+      component: "text",
+    },
+    {
+      name: "our_product_image_2",
+      component: "image",
+      uploadDir,
+      previewSrc,
+    },
+    {
+      name: "tips_title_1",
+      component: "text",
+    },
+    {
+      name: "tips_description_1",
+      component: "text",
+    },
+    {
+      name: "tips_image_1",
+      component: "image",
+      uploadDir,
+      previewSrc,
+    },
+    {
+      name: "tips_title_2",
+      component: "text",
+    },
+    {
+      name: "tips_description_2",
+      component: "text",
+    },
+    {
+      name: "tips_image_2",
+      component: "image",
+      uploadDir,
+      previewSrc,
+    },
+    {
+      name: "business_solutions",
+      component: "text",
+    },
+    {
+      name: "business_solutions_image_1",
+      component: "image",
+      uploadDir,
+      previewSrc,
+    },
+    {
+      name: "business_solutions_title_1",
+      component: "text",
+    },
+    {
+      name: "business_solutions_description_1",
+      component: "text",
+    },
   ],
 };
